86f39be437d6b8df9bb0b6230a16eeb672b32853
LeninDHP <donlino_9012@hotmail.com>
2015-05-19 18:18:42 -0500

    hecho

---
+++ preguntas/preguntas7/funciones.php
@@ -0,0 +1,56 @@
+
+<?php
+
+$paises_capitales = array(
+'Argentina' => 'Buenos Aires', 
+'Bolivia' => 'La Paz',
+'Brasil' => 'Brasilia', 
+'Chile' => 'Santiago' 
+);
+
+
+
+    echo "<table>";
+    echo "<caption>Paises en America</caption>";
+        echo "<tr>";
+        echo "<th>Paises</th>";
+        echo "<th>Capitales</th>"; 
+        echo "</tr>";
+
+$columnas = 4;
+$filas = 2;
+for($i=0; $i < $columnas; $i++)
+{
+    for($j=0; $j < $filas; $j++)
+    {
+        $matriz[$i][$j] = $i * $j;
+    }
+}
+       
+
+echo "<table>";
+//
+
+
+
+
+                          
+    ?>
+       
+    </table>
+    </body>
+
+
+</html>
+
+        
+</table>
+</div>
+</body>
+
+</html>
+
+
+
+        
+
